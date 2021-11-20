6c21921b48003a67760e5ae07f9752f3c277a303
Hazdryx <hazdryx@gmail.com>
2021-11-20 12:03:46 -0600

    Created a simple context.

--- LightStitch/LightStitchContext.cs
+++ LightStitch/LightStitchContext.cs
@@ -1,9 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+
+using Hazdryx.Drawing;
 
 namespace Hazdryx.LightStitch
 {
+    /// <summary>
+    ///     A context in which to build a LightStitch environment.
+    /// </summary>
     public class LightStitchContext
     {
+        /// <summary>
+        ///     Gets the off state image.
+        /// </summary>
+        public FastBitmap OffState;
 
+        public LightStitchContext(FastBitmap offState)
+        {
+            if (offState == null)
+            {
+                throw new ArgumentNullException(nameof(offState));
+            }
+            this.OffState = offState;
+        }
     }
 }
